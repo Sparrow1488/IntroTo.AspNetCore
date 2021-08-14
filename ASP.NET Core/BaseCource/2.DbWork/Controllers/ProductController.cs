e297c83fddaec0dfd99d0f4d6f5d697c1a171a21
Sparrow1488 <ilyaokubev@gmail.com>
2021-08-14 21:02:39 +0300

    razor pages

--- ASP.NET Core/BaseCource/2.DbWork/Controllers/ProductController.cs
+++ ASP.NET Core/BaseCource/2.DbWork/Controllers/ProductController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _2.DbWork.Database;
+using _2.DbWork.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace _2.DbWork.Controllers
 {
     [Route("/product")]
     public class ProductController : Controller
     {
+        private readonly ProductsDbContext _db;
+
+        public ProductController(ProductsDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> allProducts = _db.Products;
+            return View(allProducts);
         }
     }
 }
