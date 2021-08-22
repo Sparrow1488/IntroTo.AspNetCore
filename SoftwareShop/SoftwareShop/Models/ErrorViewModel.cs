9a0c02e0aaad642c17d314c3020744ffacb23663
Sparrow1488 <ilyaokubev@gmail.com>
2021-08-22 20:16:07 +0300

    начал первый практический проект на ASP Net MVC

---
+++ SoftwareShop/SoftwareShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoftwareShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
