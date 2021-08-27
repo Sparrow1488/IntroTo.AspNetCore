176a648d3c7cece5dec3696fffb175f538eb0e45
Sparrow1488 <ilyaokubev@gmail.com>
2021-08-27 21:40:17 +0300

    джаба_ск_епт: числа, строки

--- Bases-Of-Bootstrap/introduction-js/script.js
+++ Bases-Of-Bootstrap/introduction-js/script.js
@@ -1,23 +1,33 @@
+// Hello world
 let word = "Hello Джава-скрипт ебаный";
 console.log(word);
+
+// Работа с html
 let element = document.getElementById("id-num");
 element.hidden = true;
-
 let btn = document.getElementById("send-data-btn");
 btn.addEventListener("click", () => {
     alert("[Server]: Data received");
 });
 
+// Объекты
 const man = {
-    name: "Valentos",
-    age: 228,
-    job: "full-stack senior team-lead manager",
-    cash: 10000000
+    name: "Man",
+    age: 28,
+    sex: "Male"
 }
 console.log(man);
-man.age = 1488;
-console.log(man);
-console.log("age type is number : " + !isNaN(man.age));
-function isValidObject(){
 
-}
+//  Работа со строками
+let row = "Just string";
+console.log(`Input row: ${row}`);
+
+// Работа с числами
+const stringParse = "2";
+const parsedString = Number.parseInt(stringParse);
+console.log("Parsed string to Int: " + parsedString);
+let numArray = [ 12, 14, 18, 22, 24, 35, 99 ];
+let maxNum = Math.max(numArray);
+console.log(`The large num of ${numArray} is ${maxNum}`); // ДЖАБА СКРИПТ СОСЕТ МОЮ БОЛЬШУЮ БИБУ
+
+
