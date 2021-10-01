a7609a5a65f920341123a34100bd0a16121cd3e1
Sparrow1488 <ilyaokubev@gmail.com>
2021-10-01 20:32:06 +0300

    навел суету в файлах

--- ASP.NET Core/practice/LearnEnglish/LearnEnglish/Pages/Index.cshtml.cs
+++ ASP.NET Core/practice/LearnEnglish/LearnEnglish/Pages/Index.cshtml.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using LearnEnglish.Database;
+using LearnEnglish.Entities;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LearnEnglish.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
         {
-            _logger = logger;
+            _db = db;
         }
-
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            //_db.Database.EnsureDeleted();
+            //_db.Database.EnsureCreated();
 
+            //var profile = new Profile();
+            //profile.Login = "Sparrow";
+            //profile.Password = "1488";
+            //profile.Dictionaries = new List<WordsDictionary>();
+            //profile.Dictionaries.Add(new WordsDictionary());
+            //var res = _db.Profiles.Add(profile);
+            //await _db.SaveChangesAsync();
         }
     }
 }
--- ASP.NET Core/practice/LearnEnglish/LearnEnglish/Startup.cs
+++ ASP.NET Core/practice/LearnEnglish/LearnEnglish/Startup.cs
@@ -1,68 +1,71 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using LearnEnglish.Database;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
-using LearnEnglish.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace LearnEnglish
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options => UseSqlServer(options));
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //    options.OnAppendCookie = cookieContext =>
+            //      CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+            //    options.OnDeleteCookie = cookieContext =>
+            //      CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+            //});
+
+            //services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            //    .AddCookie(options =>
+            //    {
+            //        options.Cookie.SameSite = SameSiteMode.None;
+            //        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            //        options.Cookie.IsEssential = true;
+            //    });
+
+            //services.AddSession(options =>
+            //{
+            //    options.Cookie.SameSite = SameSiteMode.None;
+            //    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            //    options.Cookie.IsEssential = true;
+            //});
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            if (options.SameSite == SameSiteMode.None)
+                options.SameSite = SameSiteMode.Unspecified;
+        }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
             app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
             });
+        }
+
+        private DbContextOptionsBuilder UseSqlServer(DbContextOptionsBuilder builder)
+        {
+            return builder.UseSqlServer(Configuration.GetConnectionString("BaseConnection"));
         }
     }
 }
