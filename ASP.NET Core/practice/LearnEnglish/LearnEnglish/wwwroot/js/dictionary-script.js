ddd543455690a25001e179d3db25f7b67af27f3d
Sparrow1488 <ilyaokubev@gmail.com>
2021-10-05 16:41:31 +0300

    lE: добавлена возможность просматривать свой словарь

---
+++ ASP.NET Core/practice/LearnEnglish/LearnEnglish/wwwroot/js/dictionary-script.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $(".word-panel__row")
+    .find(".see-translate-btn")
+    .click(function () {
+        $(this).parent().find(".word-translate-item").slideToggle("fast");
+    });
+});
