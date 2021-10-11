45aa1f8d9fdd7e8d63c2817bcf66aed4f4437e4c
Sparrow1488 <ilyaokubev@gmail.com>
2021-10-11 21:32:51 +0300

    lE: настроил навигацию с учетом Training/Randomizer

--- ASP.NET Core/practice/LearnEnglish/LearnEnglish/Pages/Training/Randomizer.cshtml.cs
+++ ASP.NET Core/practice/LearnEnglish/LearnEnglish/Pages/Training/Randomizer.cshtml.cs
@@ -1,11 +1,30 @@
+using LearnEnglish.Database;
+using LearnEnglish.Entities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LearnEnglish.Pages.Training
 {
     public class RandomizerModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public List<WordsDictionary> Dictionaries { get; set; } = new List<WordsDictionary>();
+
+        public RandomizerModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public IActionResult OnGet()
+        {
+            IActionResult result = new RedirectResult("../Login");
+            if(Request.Cookies.TryGetValue("Login", out string login))
+            {
+                Dictionaries = _db.Dictionaries.Where(user => user.Profile.Login == login).ToList();
+                result = new PageResult();
+            }
+            return result;
         }
     }
 }
