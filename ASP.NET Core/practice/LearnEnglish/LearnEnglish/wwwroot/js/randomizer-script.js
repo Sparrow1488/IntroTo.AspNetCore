5d60ea0b29166b175ae5d5ee423a2207b7742e6f
Sparrow1488 <ilyaokubev@gmail.com>
2021-10-12 16:59:40 +0300

    lE: научился отправлять запросы через fetch

--- ASP.NET Core/practice/LearnEnglish/LearnEnglish/wwwroot/js/randomizer-script.js
+++ ASP.NET Core/practice/LearnEnglish/LearnEnglish/wwwroot/js/randomizer-script.js
@@ -1,5 +1,36 @@
 const trainingDictionaries = [];
 const startBtn = $(".start-btn");
+
+function removeArrayItem(array, item) {
+    const index = array.indexOf(item);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+}
+function displayArrayOfDictionaries(array) {
+    $(".selected-dictionaries-list").html("");
+    array.forEach(element => {
+        $(".selected-dictionaries-list").append(`<span class='selected-dictionary text'><b class='text'>${element}</b></span>`);
+        $(".selected-dictionaries-list").append("<span>         </span>");
+    });
+}
+async function receiveDictionaries(listOfId) {
+    let params = buildGetParams("identies", listOfId);
+    let url = "https://localhost:44372/Training/Randomizer?handler=DictionariesJson";
+
+    let response = await fetch(url + params);
+    let responseJson = await response.json();
+    return responseJson;
+}
+function buildGetParams(key, values) {
+    let row = "&";
+    values.forEach(function (elem) {
+        row += key + "=" + elem + "&";
+    });
+    console.log("Row", row);
+    return row;
+}
+
 $(document).ready(function () {
     $(".input-child-item").click(function (e) { 
         e.preventDefault();
@@ -19,18 +50,14 @@ $(document).ready(function () {
         }
         else startBtn.removeClass("disabled-btn");
     });
+
+    $(startBtn).click(async function () {
+        let response = await receiveDictionaries([1, 2, 3, 4]);
+        console.log(response);
+        response.values.forEach(function (elem) {
+            console.log("Value", elem.value);
+        });
+        
+    });
 });
 
-function removeArrayItem(array, item){
-    const index = array.indexOf(item);
-    if (index > -1) {
-        array.splice(index, 1);
-    }
-}
-function displayArrayOfDictionaries(array){
-    $(".selected-dictionaries-list").html("");
-    array.forEach(element => {
-        $(".selected-dictionaries-list").append(`<span class='selected-dictionary text'><b class='text'>${element}</b></span>`);
-        $(".selected-dictionaries-list").append("<span>         </span>");
-    });
-}
