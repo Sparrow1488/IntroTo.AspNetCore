5551dac6c671c91465a905df45d861d5f761f283
Sparrow1488 <ilyaokubev@gmail.com>
2021-10-12 21:33:44 +0300

    lE: прикрутил progressBar (долбаные проценты)

--- ASP.NET Core/practice/LearnEnglish/LearnEnglish/wwwroot/js/randomizer-active-script.js
+++ ASP.NET Core/practice/LearnEnglish/LearnEnglish/wwwroot/js/randomizer-active-script.js
@@ -4,24 +4,46 @@ let currentIndex = 0;
 let currentQuestionWord = trainingWords[currentIndex];
 
 function displayTask() {
-    currentQuestionWord = trainingWords[currentIndex];
-    $(numberTask).html(`Task ${currentIndex + 1}/${trainingWords.length}`);
-    $(".translate-word").html(`${currentQuestionWord.value}`);
+    if (trainingWords.length < 1 || !trainingWords) {
+        displayResults();
+    }
+    else {
+        currentQuestionWord = trainingWords[currentIndex];
+        $(numberTask).html(`Task ${currentIndex + 1}/${trainingWords.length}`);
+        $(".translate-word").html(`${currentQuestionWord.value}`);
+    }
 }
+
 function nextTask() {
     currentIndex++;
+    moveProgressBar();
     if (currentIndex < trainingWords.length) {
         displayTask();
         resetInputValue();
     }
     else displayResults();
 }
+
 function displayResults() {
     alert("FINISH");
+    setTimeout(function () {
+        window.location.reload();
+    }, 500);
 }
+
 function resetInputValue() {
     const answer = document.querySelector(".answer");
     answer.value = "";
+}
+
+function moveProgressBar() {
+    let all = trainingWords.length;
+    let current = currentIndex;
+    let kaef = all / current;
+    let result = 100 / kaef;
+    console.log(`${all}/${current} = ${kaef}`);
+    $(".progress .color").width(`${result}%`);
+    console.log(result);
 }
 
 $(document).ready(function () {
