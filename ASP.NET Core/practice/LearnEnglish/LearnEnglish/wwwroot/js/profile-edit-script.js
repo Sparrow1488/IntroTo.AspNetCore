39ab4140e08cbefe7c608eabd589e6d5bcbef671
Sparrow1488 <ilyaokubev@gmail.com>
2021-10-13 20:56:14 +0300

    lE: сверстал страницу редактирования профиля

---
+++ ASP.NET Core/practice/LearnEnglish/LearnEnglish/wwwroot/js/profile-edit-script.js
@@ -0,0 +1,21 @@
+﻿function displaySettingsPart(settingPartTitle) {
+    console.log(settingPartTitle);
+    const lowerInputPart = settingPartTitle.toLowerCase();
+    hideAllSettings();
+    if (lowerInputPart == "profile")
+        $(".profile-settings").show();
+    else if (lowerInputPart == "account")
+        $(".account-settings").show();
+}
+function hideAllSettings() {
+    $(".settings-part-item").hide();
+}
+
+$(document).ready(function () {
+    displaySettingsPart("profile");
+
+    $(".settings-btn").click(function () {
+        const settingPartTitle = $(this).html();
+        displaySettingsPart(settingPartTitle);
+    });
+});
