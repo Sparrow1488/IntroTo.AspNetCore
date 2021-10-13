4a928852d56d362f8de09150e5979de59241d0fa
Sparrow1488 <ilyaokubev@gmail.com>
2021-10-13 19:11:39 +0300

    обновил

--- ASP.NET Core/practice/LearnEnglish/LearnEnglish/Pages/Profile/Profile.cshtml.cs
+++ ASP.NET Core/practice/LearnEnglish/LearnEnglish/Pages/Profile/Profile.cshtml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LearnEnglish.Pages
+namespace LearnEnglish.Pages.Profile
 {
     public class ProfileModel : PageModel
     {
