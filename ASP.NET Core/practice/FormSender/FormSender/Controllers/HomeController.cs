92a4e77aa2311fadb294cbf8a65533332d92ac9a
Валентин <ilyaokubev@gmail.com>
2022-04-15 16:10:44 +0300

    Feature: создал проектик для тренировок

---
+++ ASP.NET Core/practice/FormSender/FormSender/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormSender.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new { message = "ok" });
+        }
+    }
+}
