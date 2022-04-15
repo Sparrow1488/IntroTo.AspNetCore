75f5473400dd2a9dd55e16895a19e101e998bd84
Валентин <ilyaokubev@gmail.com>
2022-04-15 18:57:54 +0300

    Feature: добавил новый проект для хранения всех сущностей

---
+++ ASP.NET Core/practice/FormSender/FormSender.Entities/MessageForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FormSender.Entities
+{
+    public class MessageForm
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
---
+++ ASP.NET Core/practice/FormSender/FormSender.Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FormSender.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+    }
+}
